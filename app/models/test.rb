fc23f640031a07be09c1f8cf2c8c10d7bd5f0700
Kadina1988 <kadina.aziev@mail.ru>
2023-01-19 17:22:50 +0300

    correct user.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
 
   def self.list(category_name)
-    Test.joins('JOIN categories ON categories.id = tests.category_id').where("categories.title = ?", category_name).order(title: :DESC).pluck(:title)
+    Test.joins('JOIN categories ON categories.id = tests.category_id')
+    .where(categories: { title: category_name })
+    .order(title: :DESC)
+    .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 
   def list_tests(level)
-    test_id = UserProfile.where("user_id = ?", self.id).pluck(:test_id)
-    Test.where(id: test_id).where("level = ?", level)
+    Test.joins("JOIN user_profiles ON tests.id = user_profiles.test_id ")
+    .where(user_profiles: { user_id: self.id })
+    .where(tests: { level: level })
+    .pluck(:title)
+    # test_id = UserProfile.where("user_id = ?", self.id).pluck(:test_id)
+    # Test.where(id: test_id).where("level = ?", level)
   end
 end
