1e221ad00d5d74cf4366754a034ab9493cfb0142
Kadina1988 <110830603+Kadina1988@users.noreply.github.com>
2023-01-19 17:31:19 +0300

    Update user.rb
    remove unnecessary

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,5 @@ class User < ApplicationRecord
     Test.joins("JOIN user_profiles ON tests.id = user_profiles.test_id ")
     .where(user_profiles: { user_id: self.id })
     .where(tests: { level: level })
-    .pluck(:title)
-    # test_id = UserProfile.where("user_id = ?", self.id).pluck(:test_id)
-    # Test.where(id: test_id).where("level = ?", level)
   end
 end
