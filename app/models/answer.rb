acb4cca7ee212030398de73447d749629e700c00
Kadina1988 <kadina.aziev@mail.ru>
2023-01-29 12:40:46 +0300

    add validate

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,16 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+
+  scope :right, -> { where(correct: true) }
+
+  validate :validate_count_answers, on: :create
+
+  private
+
+  def validate_count_answers
+    errors.add(:qategory_id, 'too much answers for question') if Answer.where(question_id: self.question_id).count > 3
+  end
+
 end
